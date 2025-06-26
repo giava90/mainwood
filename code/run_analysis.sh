d6bec5a2d3e9ab591ee43d09fd4ea1946b90fff6
giava90 <giava90@gmail.com>
2025-06-26 11:57:41 +0200

    ading wall time

--- code/run_analysis.sh
+++ code/run_analysis.sh
@@ -10,8 +10,9 @@ mem_per_cpu=20000 #megabytes per cpu
 case_study='All'
 management_scenario='ALL'
 folder_data='/cluster/scratch/giacomov/mainwood/'
+walltime='8:00:00'
 # Submit the job to SLURM using sbatch
 # -c specifies the number of CPU cores
 # --wrap allows executing a command within sbatch
 # --mem-per-cpu specifies the memory per cpu in megabytes
-sbatch -c $n_cores --mem-per-cpu=$mem_per_cpu --wrap="python summarize_and_create_plots.py $case_study $management_scenario $folder_data $n_cores"
+sbatch -c $n_cores --time=$walltime --mem-per-cpu=$mem_per_cpu --wrap="python summarize_and_create_plots.py $case_study $management_scenario $folder_data $n_cores"
